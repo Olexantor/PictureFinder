f40b6c415b690b6f939d74980a45a39acfb83f06
Alexander Nikolaev <nikolaev.warrior@gmail.com>
2021-12-14 20:39:59 +0500

    ManyPicturesVC
    setup Nav controller, search controller and CollectionVC

---
+++ PictureFinder/Views/ManyPicturesCell.swift
@@ -0,0 +1,13 @@
+//
+//  ManyPicturesCell.swift
+//  PictureFinder
+//
+//  Created by Александр on 14.12.2021.
+//
+
+import UIKit
+
+class ManyPicturesCell: UICollectionViewCell {
+    
+    static let identifier = "Pictures cell"
+}
